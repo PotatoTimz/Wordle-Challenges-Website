97ec8b1675c6ffc865f550ca3f53cf0dea63a702
Andre Alix <andrealix2698@gmail.com>
2024-09-14 19:32:24 -0400

    test

--- backend/api/utilities/fetchWordBank.js
+++ backend/api/utilities/fetchWordBank.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 const wordSet = fs
-  .readFileSync(path.resolve("/assets/valid-wordle-words.txt"), "utf8")
+  .readFileSync(path.join(process.cwd()))
   .toString("utf-8")
   .split("\n")
   .map((word) => {
