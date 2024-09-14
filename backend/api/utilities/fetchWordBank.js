b847b05911210b0ab3a36c422738b5739425aee7
Andre Alix <andrealix2698@gmail.com>
2024-09-14 19:39:11 -0400

    test

--- backend/api/utilities/fetchWordBank.js
+++ backend/api/utilities/fetchWordBank.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
 const path = require("path");
 
-const wordSet = fs
-  .readFileSync(path.join(process.cwd(), "valid-wordle-words.txt"))
-  .toString("utf-8")
-  .split("\n")
-  .map((word) => {
-    return word.trim();
-  });
+// const wordSet = fs
+//   .readFileSync(path.join(process.cwd(), "valid-wordle-words.txt"))
+//   .toString("utf-8")
+//   .split("\n")
+//   .map((word) => {
+//     return word.trim();
+//   });
 
 module.exports = wordSet;
